31fae3d2e58a3fc347202200ceb0c74ed14b4eca
oyin-ahmed <oyink.ahmed@gmail.com>
2025-07-06 23:28:26 +0200

    js updated to display selected city and timezone

--- src/app.js
+++ src/app.js
@@ -1,22 +1,48 @@
 function updateTime() {
   let amsterdamElement = document.querySelector("#amsterdam");
-  let amsterdamDate = amsterdamElement.querySelector(".date");
-  let amsterdamTimeElement = amsterdamElement.querySelector(".time");
-  let date = moment().format("dddd, MMMM Do YYYY");
-  let amsterdamTime = moment().tz("Europe/Amsterdam");
-  let amsterdamCurrentTime = amsterdamTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-  amsterdamDate.innerHTML = `${date}`;
-  amsterdamTimeElement.innerHTML = `${amsterdamCurrentTime}`;
-
-  let saoPauloElement = document.querySelector("#sao-paulo");
-  let saoPauloDate = saoPauloElement.querySelector(".date");
-  let saoPauloTimeElement = saoPauloElement.querySelector(".time");
-  let saoPauloTime = moment().tz("America/Sao_Paulo");
-  let saoPauloCurrentTime = saoPauloTime.format("h:mm:ss [<small>]A[</small>]");
-  saoPauloDate.innerHTML = `${date}`;
-  saoPauloTimeElement.innerHTML = `${saoPauloCurrentTime}`;
+  if (amsterdamElement) {
+    let amsterdamDate = amsterdamElement.querySelector(".date");
+    let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+    let amsterdamTime = moment().tz("Europe/Amsterdam");
+    let amsterdamCurrentTime = amsterdamTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+    amsterdamDate.innerHTML = amsterdamTime.format("dddd, MMMM Do YYYY");
+    amsterdamTimeElement.innerHTML = `${amsterdamCurrentTime}`;
+  }
+  let shanghaiElement = document.querySelector("#shanghai");
+  if (shanghaiElement) {
+    let shanghaiDate = shanghaiElement.querySelector(".date");
+    let shanghaiTimeElement = shanghaiElement.querySelector(".time");
+    let shanghaiTime = moment().tz("Asia/Shanghai");
+    let shanghaiCurrentTime = shanghaiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+    shanghaiDate.innerHTML = shanghaiTime.format("dddd, MMMM Do YYYY");
+    shanghaiTimeElement.innerHTML = `${shanghaiCurrentTime}`;
+  }
 }
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = ` <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date"> ${cityTime.format("dddd, MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", updateCity);
